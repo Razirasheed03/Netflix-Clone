b0c5ae00f4015773b91163da133db3a3c96ed5da
Razirasheed03 <razirasheed03@gmail.com>
2025-03-23 16:04:11 +0530

    Initial commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Login.css'
+import logo from '../../assets/logo.png'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <img src={logo}  className='login-logo' alt="" />
+      <div className="login-form">
+        <h1>Sign Up</h1>
+        <form>
+          <input type="text" placeholder='Your name' />
+          <input type="email" placeholder='Enter Email' />
+          <input type="password" placeholder=' Enter Password' />
+          <button>Sign Up</button>
+          <div className="form-help">
+            <div className="remember">
+              <input type="checkbox"/>
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help ?</p>
+          </div>
+        </form>
+        <div className='form-switch'>
+          <p>New to Netflix <span>Sign Up Now</span></p>
+          <p>Already have Account <span>Sign In Now</span></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './player.css'
+
+const Player = () => {
+  return (
+    <div>Player</div>
+  )
+}
+
+export default Player

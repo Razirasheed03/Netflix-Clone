fc0275cc9a4ee9c8cc432a24925b4fd172775bce
Razirasheed03 <razirasheed03@gmail.com>
2025-03-23 16:51:03 +0530

    login page completed

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
 
@@ -6,13 +6,29 @@ import cards_data from '../../assets/cards/Cards_data'
 
 
 const TitleCards = ({title,category}) => {
-
+  
+  const [apiData,setApiData]=useState([])
   const cardsRef=useRef();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjcyZmNjMzc2OThkMWZiNmRjZmIxZmQ2YzVhMTQ3OCIsIm5iZiI6MTc0MjcyNzUxMS41ODYsInN1YiI6IjY3ZGZlOTU3ZTRhOWM4NjkwNjA3Y2ZmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oaHzzKfmeLv8qJrlkkLwR2-avKRuAuKc-PIL2Wnsbew'
+    }
+  };
+  
+ 
+
 const handleWheel=(event)=>{
   event.preventDefault();
   cardsRef.current.scrollLeft+=event.deltaY;
 }
 useEffect(()=>{
+  fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.error(err));
   cardsRef.current.addEventListener('wheel',handleWheel)
 },[]) 
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
           
           <input type="email" placeholder='Enter Email' />
           <input type="password" placeholder=' Enter Password' />
-          <button>Sign Up</button>
+          <button>{signState}</button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -25,8 +25,9 @@ const Login = () => {
           </div>
         </form>
         <div className='form-switch'>
-          <p>New to Netflix <span>Sign Up Now</span></p>
-          <p>Already have Account <span>Sign In Now</span></p>
+          {signState==='Sign In'?
+           <p>New to Netflix <span onClick={()=>setSignState("Sign Up")}>
+            Sign Up Now</span></p>: <p>Already have Account <span onClick={()=>setSignState("Sign In")}>Sign In Now</span></p>}
         </div>
       </div>
     </div>

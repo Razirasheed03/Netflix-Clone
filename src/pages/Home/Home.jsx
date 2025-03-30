98743b414ad0146197dc1a03dfd9fcb27ed8a288
Razirasheed03 <razirasheed03@gmail.com>
2025-03-31 01:00:56 +0530

    home page fixes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,27 +10,27 @@ import Footer from '../../components/Footer/Footer'
 const Home = () => {
   return (
     <div className='home'>
-      <Navbar/>
+      <Navbar />
       <div className="hero">
         <img src={hero_banner} alt="" className='banner-img' />
         <div className="hero-caption">
-          <img src={hero_title} alt="" className='caption-img'/>
+          <img src={hero_title} alt="" className='caption-img' />
           <p>Dicovering his ties to a secret ancient order,Lorem
-             ipsum dolor, sit amet consectetur adipisicing elit. Pariatur doloremque explicabo optio, alias aliquid blanditiis!</p>
-             <div className="hero-btns">
-              <button className='btn'><img src={play_icon} alt="" />Play</button>
-              <button className='btn dark-brn'><img src={info_icon} alt="" />More Info</button>
-             </div>
-             <TitleCards/>
+            ipsum dolor, sit amet consectetur adipisicing elit. Pariatur doloremque explicabo optio, alias aliquid blanditiis!</p>
+          <div className="hero-btns">
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-brn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+          <TitleCards />
         </div>
       </div>
       <div className="more-cards">
-        <TitleCards title={'Blockbuster Movies'} category={"top_rated"}/>
-        <TitleCards title={'Only on Netflix'} category={"popular"}/>
-        <TitleCards title={'Upcoming'} category={"upcoming"}/>
-        <TitleCards title={'Top pics for you'} category={"now_playing"}/>
+        <TitleCards title={'Blockbuster Movies'} category={"top_rated"} />
+        <TitleCards title={'Only on Netflix'} category={"popular"} />
+        <TitleCards title={'Upcoming'} category={"upcoming"} />
+        <TitleCards title={'Top pics for you'} category={"now_playing"} />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
